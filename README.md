dccfb8e84597cacca6114dba7701901a8a76ac3c
Edson Rodrigues <thecodeverse12@gmail.com>
2024-07-10 12:52:56 +0100

    Upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# Image to ASCII Converter
+
+This project is a python script that converts as image into an ASCII art representation. It uses the Python Imaging Library (PIL) to manipulate the image and convert it into a grayscale image, which is then converted into an ASCII art representation using a predefined set of ASCII characters.
+
+## ⚙️ Installation
+
+To run this project, you need to have Python installed on your system. You can download the latest version of Python from the official Python website.
+
+Additionally, you need to install the Python Imaging Library (PIL) using pip:
+```bash
+pip install pillow
+```
+
+## 💡 Usage
+
+To use this script, simply run the `main.py` file and enter the path to the image you want convert when prompted.
+
+## 📝 Notes
+
+- The ASCII art representation is generated using a predefined set of ASCII characters. You can improve this by allowing the user to customize the set of characters used or by using a more advanced algorithm to generate the ASCII art.  
+- The script does not handle errors well. You can improve this by adding more robust error handling and providing more informative error messages.
+
+## 👷‍♂️ Author 
+
+This project was created by **Edson Rodrigues (Imacod3r)**.
---
+++ main.py
@@ -0,0 +1,45 @@
+from PIL import Image
+import os
+
+ascii_chars = ["#", "@", "%", "*", "+", "=", "-", ":", ".", " "]
+
+# Resize image
+def ResizeImage(image, new_width=200):
+    width, height = image.size
+    ratio = height // width
+    new_height = new_width * ratio
+    return image.resize((new_width, new_height))
+
+# Convert to grayscale
+def ConvertToGrayscale(image):
+    return image.convert("L")
+
+# Read pixels
+def PixelsToAscii(image):
+    pixels = list(image.getdata())
+    ascii_string = "".join([ascii_chars[pixel // 32] for pixel in pixels])
+    img_width = image.width
+    ascii_image = "\n".join([ascii_string[index:index + img_width] for index in range(0, len(ascii_string), img_width)])
+    return ascii_image
+    
+# Return ASCII Image
+def ImgToAscii(path):
+    # Tries to open the image
+    try:
+        image = Image.open(path)
+    except:
+        print("INVALID PATH!")
+        return
+    # Tries to convert the image to ascii
+    try:
+        ascii_image = PixelsToAscii(ConvertToGrayscale(ResizeImage(image)))
+        return ascii_image
+    except ValueError as ve:
+        print(str(ve).upper())
+
+if __name__ == "__main__":
+    path = input("TYPE THE IMAGE'S PATH:")
+    ascii_image = ImgToAscii(path)
+    if ascii_image:
+        os.system("cls||clear")
+        print(ascii_image)
